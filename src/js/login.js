4dc189462a83983d42ea60b81fedcc92b8708e44
doris <Dragon_long163@163.com>
2021-01-07 10:34:59 +0800

    loginjs

--- src/js/login.js
+++ src/js/login.js
@@ -1,31 +1,53 @@
-// console.log($('.mr-nav1').children())
-$('.mr-nav1').children().click(function(){
-    $(this)
-    .addClass('action')
-    .siblings()
-    .removeClass('action')
-    .parent()
-    .next()
-    .children()
-    .removeClass('active')
-    .eq($(this).index())
-    .addClass('active')
+(function(){
+    // console.log($('.mr-nav1').children())
+    // 切换选项卡
+    $('.mr-nav1').children().click(function(){
+        $(this)
+        .addClass('action')
+        .siblings()
+        .removeClass('action')
+        .parent()
+        .next()
+        .children()
+        .removeClass('active')
+        .eq($(this).index())
+        .addClass('active')
+    });
+    // 动画切换二维码
+    $('.erweima').mouseenter(function () { 
+        $(this)
+        .children('.saoma').animate({
+            left:20
+        },300,'linear')
+        .next()
+        .fadeIn(900,'linear')
+    }).mouseleave(function () { 
+        $(this)
+        .children('.saoma')
+        .animate({
+            left: 96
+        },500,'linear')
+        .next()
+        .css('display','none')
+        .finish()
+    });
 
-})
+    // 登录
+    $('.login-btn').click(function(){
+        $.ajax({
+            url:"../json/login.json",
+            data:{
+                username: $('#loginname').val(),
+                password: $('#nloginpwd').val()
+            },
+            dataType:"json",
+            success:function(res){
+                if(res.code === 1){
+                    localStorage.setItem('username',$('#loginname').val());
+                    location.href = "../pages/first.html"
+                }
+            }
+        })
+    })
+})()
 
-$('.erweima').mouseover(function () { 
-    $(this)
-    .children('.saoma').animate({
-        left:20
-    },500,'linear')
-    .finish()
-    .next()
-    .css('display','block')
-}).mouseout(function () { 
-    $(this)
-    .children('.saoma').finish().animate({
-        left: 96
-    },500,'linear')
-    .next()
-    .css('display','none')
-});
