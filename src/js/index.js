6b2651d7192695ba6d293f8501d5ef4f7aa36a35
doris <Dragon_long163@163.com>
2021-01-07 21:32:58 +0800

    index提交

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@
     // 2.定位划过二级菜单
     $('.fl').mouseenter(function () { 
         $.ajax({
-            url: "../json/shengfen.json",
+            url: "./src/json/shengfen.json",
             dataType: "json",
             success: function (res) {
                 // console.log(res)
@@ -21,5 +21,57 @@
         });
     });
 
-    // 3.
+    // 3. 侧边导航栏
+    $('.cate_menu_item').mouseenter(function () { 
+        $('.con-left-box').show()
+        // $.ajax({
+        //     url: "./src/json/cebianlan.json",
+        //     dataType: "json",
+        //     success: function (res) {
+        //         // console.log(res)
+        //         $('.con-left-box').empty()
+        //         $(res).each(function(index,element){
+        //             let ele = JSON.stringify(element)
+        //             // console.log(ele)
+        //             $('.con-left-box').append(`
+        //             <ol class="con-le-second">
+        //                 <li>
+        //                     <span>${ele}</span>
+        //                 </li>
+        //             </ol>
+        //             `)
+        //         })
+        //     }
+        // });
+    }).mouseleave(function () { 
+        $('.con-left-box').hide()
+    });
+
+    $('.con-left-box').mouseenter(function () {
+        $(this).show()
+    }).mouseleave(function () { 
+        $('.con-left-box').hide()
+    });
+
+    // 轮播图
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true, // 循环模式选项
+        
+        // 如果需要分页器
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 'auto',
+        loopedSlides: 8,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+      })
 })()
